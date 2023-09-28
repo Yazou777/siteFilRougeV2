af35656877a72d65a8f3dffd1167de8c5bfb293f
Yazou777 <wilga.kevin@sfr.fr>
2023-09-28 17:20:57 +0200

    chemin de fer detail produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -21,9 +21,8 @@ class ProduitController extends AbstractController
        $catParentId = $categorieRepository->findOneBy([ "cat_nom" => $cat_nom])->getCatParent()->getId();
        $catParentName= $categorieRepository->findOneBy(['id' => $catParentId])->getCatNom();
        $nbProduit = $produitRepository->nbProduit($cat);
-       //$x= $nbProduit[0][1];
-     
-     // dd($x);
+       //$x= $nbProduit[0][1];    
+       // dd($nbProduit);
         return $this->render('produit/index.html.twig', [
             // 'Produit' => $Produit,
             'findby' => $findby,
@@ -38,30 +37,32 @@ class ProduitController extends AbstractController
     }
 
     #[Route('/detail_produit/{pro_nom}', name: 'app_detail')]
-    public function detail(ProduitRepository $produitRepository, $pro_nom): Response
+    public function detail(ProduitRepository $produitRepository,CategorieRepository $categorieRepository , $pro_nom): Response
     {
+        //on recupére les info du produit
         $proId = $produitRepository->findOneBy([ "pro_nom" => $pro_nom])->getId();
         $pro = $produitRepository->findOneBy([ "id" => $proId]);
+        //on recupére la description du produit et on le decoupe en tableau pour l'afficher correctement
         $Description =  $produitRepository->findOneBy(['pro_nom' => $pro_nom])->getProDescription();
-        //dd($Description);
         $description = explode('*',$Description);
-       // dd($pro);
-        //$SousCategorie = $produit->getSousCategorie();
+        //on recupere la categorie du produit
+        $proCatId = $produitRepository->findOneBy([ "pro_nom" => $pro_nom])->getCat()->getId();
+        //on recupere le nom de la categorie
+        $catNom = $categorieRepository->findOneBy([ "id" => $proCatId])->getCatNom();
+        //on recupere l'id cat_parent_id puis le nom cat_nom
+        $catParentNom = $categorieRepository->findOneBy([ "id" => $proCatId])->getCatParent()->getCatNom();
+
+        //dd($catParentId);
        
-        //$description = explode(":", $produit-> getDescription());
-        //  dd($description);
         return $this->render('produit/detail.html.twig', [
             'pro' => $pro,
             'description' => $description,
-            // 'description' => $description,
-            // 'Produit' => $produit,
-            // "SousCategorie" => $SousCategorie,
-            // "chemin_de_fer" => [
-            //     // ["name" => "Accueil", "link" => "/"],
-            //     ["name" => $SousCategorie->getCategorie()->getNom(), "link" => "/sous_categorie/".$SousCategorie->getCategorie()->getId()],
-            //     ["name" => $produit->getSousCategorie()->getNom(), "link" => "/produit/".$produit->getSousCategorie()->getId()],
-            //     ["name" => $produit->getNom(), "link" => " "],
-            // ]
+            "chemin_de_fer" => [
+                 ["name" => $catParentNom, "link" => "/accueil/".$catParentNom],
+                  ["name" => $catNom, "link" => "/produit/".$catNom],
+                  ["name" => $pro_nom, "link" => "/detail_produit/".$pro_nom],
+            ]
+            
         ]);
     }
 }
