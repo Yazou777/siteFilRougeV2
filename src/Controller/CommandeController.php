3027fe780a84ed8915202909d4f93d5c236445f5
Yazou777 <wilga.kevin@sfr.fr>
2023-10-11 17:58:06 +0200

    form adresse livraison

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Panier;
 use App\Entity\Commande;
-use App\Repository\ProduitRepository;
+use App\Form\CommandeType;
 use Doctrine\ORM\EntityManager;
+use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -15,8 +17,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/commande', name: 'app_commande_')]
 class CommandeController extends AbstractController
 {
-    #[Route('/ajout', name: 'add')]
-    public function add(SessionInterface $session, ProduitRepository $produitRepository, EntityManagerInterface $em): Response
+    #[Route('/', name: 'index')]
+    public function index(SessionInterface $session, ProduitRepository $produitRepository): Response
+    {
+        $panier = $session->get('panier', []);
+        // form pre remplie
+        $commande = new Commande;
+        $commandeData = $commande->setComAdresseLivraison($this->getUser()->getUtiRue()." ".$this->getUser()->getUtiVille()." ".$this->getUser()->getUtiCodePostal()." ".$this->getUser()->getUtiPays());
+        //dd($y->getComAdresseLivraison());
+        $commande->setComAdresseLivraison($commandeData->getComAdresseLivraison());
+        $commande->setComAdresseFacturation($commandeData->getComAdresseLivraison());
+        $form = $this->createForm(CommandeType::class, $commande);
+
+        // vide le panier
+        //$session->set('panier', []);
+
+        // On initialise des variables
+        $data = [];
+        $total = 0;
+        $totalQte = 0;
+        foreach($panier as $id => $quantity){
+            $produit = $produitRepository->find($id);
+
+            $data[] = [
+                'produit' => $produit,
+                'quantite' => $quantity
+            ];
+            $total += $produit->getProPrix() * $quantity;
+            $totalQte +=  $quantity;
+        }
+        return $this->render('commande/index.html.twig',compact('data','total','totalQte','form'));
+    }
+
+
+
+
+
+    #[Route('/ajout', name: 'add', methods: ['POST'])]
+    public function add(SessionInterface $session, ProduitRepository $produitRepository, EntityManagerInterface $em, Request $request): Response
     {
         //S'assurer que l'utilisateur soit connecté
         $this->denyAccessUnlessGranted('ROLE_USER');
@@ -28,14 +66,29 @@ class CommandeController extends AbstractController
         return $this->redirectToRoute('app_accueil');
     }
 
+    $form = $this->createForm(CommandeType::class, data: null);
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        
+            $adresseLivraison = $form->get('com_adresse_livraison')->getData();
+            $adresseFacture = $form->get('com_adresse_facturation')->getData();
+            $commentaire = $form->get('com_commentaire')->getData();
+            //dd($adresseLivraison);
+            // $em->persist($commande);
+            // $em->flush();
+
+           // return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
+        
      //Le panier n'est pas vide, on crée la commande
      $commande = new Commande();
 
      // On remplit la commande
      $commande->setComUti($this->getUser());
-     $commande->setComAdresseLivraison($this->getUser()->getUtiRue()." ".$this->getUser()->getUtiVille()." ".$this->getUser()->getUtiCodePostal()." ".$this->getUser()->getUtiPays());
-     //$order->setReference(uniqid());
-
+     //$commande->setComAdresseLivraison($this->getUser()->getUtiRue()." ".$this->getUser()->getUtiVille()." ".$this->getUser()->getUtiCodePostal()." ".$this->getUser()->getUtiPays());
+     //dd($commande);
+     $commande->setComAdresseFacturation($adresseFacture);
+     $commande->setComAdresseLivraison($adresseLivraison);
+     $commande->setComCommentaire($commentaire);
      // On parcourt le panier pour créer les détails de commande
      foreach($panier as $proId => $quantite){
          $panier = new Panier();
@@ -55,7 +108,7 @@ class CommandeController extends AbstractController
      // On persiste et on flush
      $em->persist($commande);
      $em->flush();
-
+    }
        $session->remove('panier');
 
         $this->addFlash('message', 'Commande créée avec succès');
